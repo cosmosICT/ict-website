657acbc9f3fb7fd4a1140aed025f51d705227d7b
Lucent <lucentthulung@gmail.com>
2023-10-20 14:03:35 +0545

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -10,42 +10,47 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
-  plugins: [
+	plugins: [
 		new CopyPlugin({
-      patterns: [
-        {
-          context: path.resolve(__dirname, "dist"),
-          from: path.join(__dirname, '/src/index.html'),
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
+			patterns: [
+				{
+					context: path.resolve(__dirname, "dist"),
+					from: path.join(__dirname, '/src/index.html'),
+				},
+			],
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+	],
 	module: {
 		rules: [
 			{
-        test: /\.html$/i,
-        type: "asset/resource",
-      },
+				test: /\.html$/i,
+				type: "asset/resource",
+			},
 			{
 				test: /\.css$/i,
 				use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'],
 			},
 			{
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
 		],
 	},
 	optimization: {
-    minimizer: [
-			new HtmlMinimizerPlugin(),
-      new CssMinimizerPlugin(),
-    ],
-  },
+		minimizer: [
+			new HtmlMinimizerPlugin({
+				minimizerOptions: {
+					collapseWhitespace: true,
+					removeComments: true,
+				},
+			}),
+			new CssMinimizerPlugin(),
+		],
+	},
 	mode: 'production',
 	devtool: 'source-map'
 };
